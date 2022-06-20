50844f020bf8a4257d87c6a79db16024899e8b3e
Hensetsu <nothile1@gmail.com>
2022-06-20 20:22:38 +0100

    Used redux to update email input and create input slice

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,17 +1,39 @@
 import './Auth.scss';
+import { inputActions } from '../store/index'
+
+// Import use selector to give us access to our store
+import { useSelector, useDispatch } from 'react-redux';
 
 const Auth = () => {
+
+  // Get our state slice for our inputs we defined in our redux reducer
+  const input = useSelector(state => state.input);
+
+  // Grabbing out dispatch hook
+  const dispatch = useDispatch(); 
+
+  const updateEmailHandler = (event) => {
+    event.preventDefault();
+    dispatch(inputActions.updateEmail(event.target.value));
+  }
+
   return (
     <main className="auth">
       <section>
         <form>
           <div className="control">
             <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <input 
+              type='email' 
+              id='email' 
+              autoComplete='username' 
+              onChange={updateEmailHandler}
+              value={input.email}
+              />
           </div>
           <div className="control">
             <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <input type='password' id='password' autoComplete='current-password'/>
           </div>
           <button>Login</button>
         </form>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
+const initialInput = { email: '', password: '' };
 
+// Creating a state slice for our counter on the app
 const counterSlice = createSlice({
     name: 'counter',
     initialState: initialState,
     reducers: {
-        increment(state) {  state.counter++; console.log(state.counter); },
+        increment(state) {  state.counter++; },
         decrememt(state) {  state.counter--; },
         increase(state, action) { state.counter = state.counter + action.payload.amount; },
         decrease(state, action) { state.counter = state.counter - action.payload.amount; },
@@ -15,11 +17,26 @@ const counterSlice = createSlice({
     }
 });
 
-// Creating our redux store based on our reducer function, our state will now be stored there
+// Creating a second state slice for our counter on the app
+// We're only updating our state using a payload here since we're padding event.target.value and not an object
+const inputSlice = createSlice({
+    name: 'input',
+    initialState: initialInput,
+    reducers: {
+        updateEmail(state, action){ state.email = action.payload; },
+        updatePassword(state, action){ state.password = action.payload.value; }
+    }
+});
+
+// Creating our redux store based on our state slices. We can reference these using useSelector
 const store = configureStore({
-    reducer: { counter: counterSlice.reducer }
+    reducer: { 
+        counter: counterSlice.reducer,
+        input: inputSlice.reducer 
+    }
 });
 
 // Export out store so we can subscribe components to it. Subscribed components re-evaluate with the previous state and action passed to it
 export default store;
 export const counterActions = counterSlice.actions;
+export const inputActions = inputSlice.actions;
