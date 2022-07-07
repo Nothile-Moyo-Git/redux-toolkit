2a3451867d0e085afd4c6f78a0291fd5296acfcf
Hensetsu <nothile1@gmail.com>
2022-07-07 19:20:57 +0100

    Updating cart in the backend currently

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Products from './components/Shop/Products';
 import './styles/App.scss';
 import { useEffect } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from './store/ui-slice';
 
 function App() {
+
+  const dispatch = useDispatch(); 
 
   // Import our show cart variable from our state slice
   const showCart = useSelector(state => state.ui.cartIsVisible);
@@ -14,10 +17,27 @@ function App() {
   const cart = useSelector( state => state.cart );
 
   useEffect(() => {
-    fetch('https://redux-toolkit-1c97f-default-rtdb.europe-west1.firebasedatabase.app/.json')
-    .then(response => response.json())
-    .then();
-  },[]);
+
+    const sendCartData = async () => {
+
+      const response = await fetch('https://redux-toolkit-1c97f-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+
+      if(!response.ok){
+        throw new Error('Sending cart data failed');
+      }
+
+      const message = await response.json();
+      const status = response.status;
+      const title = response.statusText;
+
+    }
+
+    sendCartData();
+
+  },[cart]);
 
   return (
     <section className="backdrop">
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,13 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { cartIsVisible: false, };
+const initialState = { cartIsVisible: false, notification: null };
 
 const uiSlice = createSlice({
     name: 'ui',
     initialState: initialState,
     reducers:{
         toggle(state){ state.cartIsVisible = !state.cartIsVisible; },
-        setFalse(state){ state.cartIsVisible = false; }
+        setFalse(state){ state.cartIsVisible = false; },
+        showNotification(state, action){
+            state.notification = { 
+                status: action.payload.status,  
+                title: action.payload.title,
+                message: action.payload.message
+            }
+        }
     }
 });
 
